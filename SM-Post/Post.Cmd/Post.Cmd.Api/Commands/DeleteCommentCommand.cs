a26dd2ba92ab8f667f76c81d6d044e473b2c9216
George <nuriszka@gmail.com>
2026-08-15 08:43:23 +0200

    setting up messages and events

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/DeleteCommentCommand.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands;
+
+public class DeleteCommentCommand : BaseCommand
+{
+    public string Username { get; set; }
+}
---
+++ SM-Post/Post.Common/Events/PostLikedEvent.cs
@@ -0,0 +1,7 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class PostLikedEvent(): BaseEvent(nameof(PostLikedEvent))
+{
+}
