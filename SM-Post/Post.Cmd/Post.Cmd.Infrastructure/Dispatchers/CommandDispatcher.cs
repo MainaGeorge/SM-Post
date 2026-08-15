1d9651da53239af3271932f1bed3049e25c4bce2
George <nuriszka@gmail.com>
2026-08-15 11:38:53 +0200

    implemented IcommandDispatcher and its implementation

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers;
+
+public class CommandDispatcher : ICommandDispacher
+{
+    private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = [];
+
+    public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+    {
+        var type = typeof(T);
+
+        if (_handlers.ContainsKey(type))
+            throw new IndexOutOfRangeException($"You can not register the same handler twice (handler for {type.Name}");
+
+        _handlers.Add(type, x => handler((T)x));
+    }
+
+    public async Task SendAsync(BaseCommand command)
+    {
+        if (_handlers.TryGetValue(command.GetType(), out var handler))
+            await handler(command);
+        else
+            throw new ArgumentNullException(nameof(handler), "No command handler was registered!");
+    }
+}
