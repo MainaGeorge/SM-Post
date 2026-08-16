f7bebd91a1a043f767af543d995d05b132182635
George <nuriszka@gmail.com>
2026-08-16 15:37:38 +0200

    added event consumer

---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Consumers/ConsumerHostedService.cs
@@ -0,0 +1,28 @@
+﻿using CQRS.Core.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Post.Query.Infrastructure.Consumers;
+
+public class ConsumerHostedService(ILogger<ConsumerHostedService> logger, IServiceProvider serviceProvider) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Event Consumer Service Starting..");
+        using var scope = serviceProvider.CreateScope();
+        var eventConsumer = scope.ServiceProvider.GetRequiredService<IEventConsumer>();
+        var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+
+        Task.Run(() => eventConsumer.Consume(topic), cancellationToken);
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Event Consumer Service Stopping..");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Consumers/EventConsumer.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using CQRS.Core.Consumers;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using Post.Query.Infrastructure.Converters;
+using Post.Query.Infrastructure.Handlers;
+using System.Text.Json;
+
+namespace Post.Query.Infrastructure.Consumers;
+
+public class EventConsumer(IOptions<ConsumerConfig> config, IEventHandler eventHandler) : IEventConsumer
+{
+    public Task Consume(string topic)
+    {
+        using var consumer = new ConsumerBuilder<string, string>(config.Value)
+            .SetKeyDeserializer(Deserializers.Utf8)
+            .SetValueDeserializer(Deserializers.Utf8)
+            .Build();
+
+        consumer.Subscribe(topic);
+
+        while (true)
+        {
+            var consumerResult = consumer.Consume();
+
+            if (consumerResult?.Message == null) continue;
+
+            var options = new JsonSerializerOptions { Converters = { new EventJsonConverter() } };
+
+            var @event = JsonSerializer.Deserialize<BaseEvent>(consumerResult.Message.Value, options);
+
+            var handlerMethod = eventHandler.GetType().GetMethod("On", [@event.GetType()]);
+
+            if (handlerMethod is null)
+                throw new ArgumentNullException(nameof(handlerMethod), "could not find handler event method!");
+
+            handlerMethod.Invoke(eventHandler, [@event]);
+
+            consumer.Commit(consumerResult);
+        }
+    }
+}
