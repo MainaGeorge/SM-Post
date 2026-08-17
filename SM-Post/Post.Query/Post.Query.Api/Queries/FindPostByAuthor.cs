9f59c76181933d1e55645fc9b9fc61f7ecdd6380
George <nuriszka@gmail.com>
2026-08-17 19:52:29 +0200

    added query dispatcher (mediator for queries)

---
+++ SM-Post/Post.Query/Post.Query.Api/Queries/FindPostByAuthor.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Queries;
+
+namespace Post.Query.Api.Queries;
+
+public class FindPostByAuthor : BaseQuery
+{
+    public string Author { get; set; }
+}
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Dispatchers/QueryDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using CQRS.Core.Infrastructure;
+using CQRS.Core.Queries;
+using Post.Query.Domain.Entitites;
+
+namespace Post.Query.Infrastructure.Dispatchers;
+
+public class QueryDispatcher : IQueryDispatcher<PostEntity>
+{
+    private readonly Dictionary<Type, Func<BaseQuery, Task<List<PostEntity>>>> _handlers = [];
+    public void RegisterHandler<TQuery>(Func<TQuery, Task<List<PostEntity>>> handler) where TQuery : BaseQuery
+    {
+        var type = typeof(TQuery);
+
+        if(_handlers.ContainsKey(type))
+            throw new InvalidOperationException($"You can not register the same handler twice (handler for {type.Name}");
+
+        _handlers.Add(type, x => handler((TQuery)x));
+
+    }
+
+    public async Task<List<PostEntity>> SendAsync(BaseQuery query)
+    {
+        var type = query.GetType();
+
+        if (_handlers.TryGetValue(type, out var handler))
+            return await handler(query);
+
+        throw new ArgumentNullException(nameof(type.Name), "No query handler was registered!");
+    }
+}
